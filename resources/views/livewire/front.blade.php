0622f6ebd88fb20f801de66c4e94cbb8f76cb632
Arifia Kasastra R <arka.progammer@gmail.com>
2020-09-16 00:04:49 +0700

    Add Front template

---
+++ resources/views/livewire/front.blade.php
@@ -0,0 +1,16 @@
+<div class="s130">
+    <form>
+      <div class="inner-form">
+        <div class="input-field first-wrap">
+          <div class="svg-wrapper">
+            <h2><b>S</b></h2>
+          </div>
+          <input id="search" type="text" placeholder="Masukkan URL" />
+        </div>
+        <div class="input-field second-wrap">
+          <button class="btn-search" type="button">Short!</button>
+        </div>
+      </div>
+      <span class="info" style="font-weight: bold">Short it as you want</span>
+    </form>
+  </div>
