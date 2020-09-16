4dca660357ec1ab323943e919f8d24f78d26f850
Arifia Kasastra R <arka.progammer@gmail.com>
2020-09-17 06:30:37 +0700

    Creating Custom URL Feature

--- app/Http/Livewire/Dashboard/Home.php
+++ app/Http/Livewire/Dashboard/Home.php
@@ -9,6 +9,7 @@ use App\Models\Link;
 class Home extends Component
 {
     public $url;
+    public $custom_url;
 
     public function render()
     {
@@ -24,12 +25,13 @@ class Home extends Component
             $link->name = "";
             do {
                 $link->short_url = Str::random(6);
-                $checklink = Link::where('short_url', $link->short_url)->first();
+                $checklink = Link::whereRaw('BINARY short_url = "' .$link->short_url. '"')->first();
             } while ($checklink);
             $link->istransit = 1;
             $link->views = 0;
             $link->save();
             $this->url = $this->getUrl() . $link->short_url;
+            $this->custom_url = $link->short_url;
             session()->flash('sukses', 'URL Berhasil Dipendekkan!');
         } else {
             session()->flash('gagal', 'Gagal, Masukkan URL terlebih dahulu');
@@ -45,5 +47,33 @@ class Home extends Component
         array_pop($url);
         $url = implode('/', $url); 
         return $url.'/';
+    }
+
+    public function customURL()
+    {
+        $short_url = explode('/', $this->url);
+        $short_url = $short_url[count($short_url)-1];
+        if($short_url!==$this->custom_url) {
+            if ($this->custom_url) {
+                $link = Link::whereRaw('BINARY short_url = "' .$short_url. '"')->first();
+                if($link) {
+                    $check_url = Link::whereRaw('BINARY short_url = "' .$this->custom_url. '"')->first();
+                    if(!$check_url) {
+                        $link->short_url = $this->custom_url;
+                        $link->save();
+                        $this->url = $this->getUrl() . $link->short_url;
+                        session()->flash('sukses', 'URL Berhasil Diubah!');
+                    } else {
+                        session()->flash('gagal', 'Gagal, URL sudah Digunakan!');
+                    }
+                } else {
+                    session()->flash('gagal', 'Gagal, URL Tidak Ditemukan!');
+                }
+            } else {
+                session()->flash('gagal', 'Gagal, Masukkan URL terlebih dahulu!');
+            }
+        } else {
+            session()->flash('sama', 'Gagal, URL dan Custom URL tidak boleh sama!');
+        }
     }
 }
--- resources/views/livewire/dashboard/home.blade.php
+++ resources/views/livewire/dashboard/home.blade.php
@@ -7,33 +7,74 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
+                    <div class="card-header">
+                        <h4>Short URL</h4>
+                    </div>
                     <div class="card-body">
                         <div class="form-group">
                             <div class="input-group mb-3">
-                                <input wire:model.defer="url" type="text" class="form-control" placeholder="Masukkan URL" aria-label="">
+                                <input id="url" wire:model.defer="url" type="text" class="form-control" placeholder="Masukkan URL" aria-label="">
                                 <div class="input-group-append">
                                     <button id="btn" class="btn btn-primary" wire:click="short" type="button">Short!
-                                        <span wire:loading class="fas fa-spinner"></span>
+                                        <span wire:loading wire:target="short" class="fas fa-spinner"></span>
                                     </button>
                                 </div>
                             </div>
-                            @if (session()->has('sukses'))
-                                <div id="result" class="alert alert-success alert-dismissible show fade">
-                                    <div class="alert-body">
-                                    <button class="close" data-dismiss="alert">
-                                        <span>×</span>
-                                    </button>
-                                    {{ session('sukses') }}
+                            @if (session()->has('sama'))
+                                <div class="form-group">
+                                    <a id="open_url" target="_blank" href="{{$url}}" class="btn btn-success btn-icon icon-left">
+                                        <i class="fas fa-link"></i> {{$url}}
+                                    </a>
+                                    <a href="#" class="btn btn-warning" onclick="custom_url()">Custom URL</a>
+                                </div>
+                                <div id="input_custom_url" class="form-group" style="display: none">
+                                    <div class="input-group mb-3">
+                                        <input wire:model.defer="custom_url" id="custom_url" type="text" class="form-control" placeholder="Ubah URL" aria-label="">
+                                        <div class="input-group-append">
+                                            <button id="btn" class="btn btn-primary" wire:click="customURL" type="button">Simpan!
+                                                <span wire:loading wire:target="customURL" class="fas fa-spinner"></span>
+                                            </button>
+                                            <button id="btn-cancel" onclick="cancelCustom()" class="btn btn-secondary">Batal</button>
+                                        </div>
                                     </div>
                                 </div>
                             @endif
-                            @if(session()->has('gagal'))
+                            @if (session()->has('sukses'))
+                                <div id="result">
+                                    <div class="form-group">
+                                        <a id="open_url" target="_blank" href="{{$url}}" class="btn btn-success btn-icon icon-left">
+                                            <i class="fas fa-link"></i> {{$url}}
+                                        </a>
+                                        <a href="#" class="btn btn-warning" onclick="custom_url()">Custom URL</a>
+                                    </div>
+                                    <div id="input_custom_url" class="form-group" style="display: none">
+                                        <div class="input-group mb-3">
+                                            <input wire:model.defer="custom_url" id="custom_url" type="text" class="form-control" placeholder="Ubah URL" aria-label="">
+                                            <div class="input-group-append">
+                                                <button id="btn" class="btn btn-primary" wire:click="customURL" type="button">Simpan!
+                                                    <span wire:loading wire:target="customURL" class="fas fa-spinner"></span>
+                                                </button>
+                                                <button id="btn-cancel" onclick="cancelCustom()" class="btn btn-secondary">Batal</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="alert alert-success alert-dismissible show fade">
+                                        <div class="alert-body">
+                                            <button class="close" data-dismiss="alert">
+                                                <span>×</span>
+                                            </button>
+                                            {{ session('sukses') }}
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                            @if(session()->has('gagal') || session()->has('sama'))
                                 <div id="result" class="alert alert-danger alert-dismissible show fade">
                                     <div class="alert-body">
                                     <button class="close" data-dismiss="alert">
                                         <span>×</span>
                                     </button>
-                                    {{ session('gagal') }}
+                                    {{ session('gagal') ?? session('sama') }}
                                     </div>
                                 </div>
                             @endif
@@ -47,6 +88,16 @@
 
 @section('scripts')
     <script>
+        function custom_url() {
+            $('#input_custom_url').css("display","inherit");
+            $('#url').prop('disabled', true);
+            $('#btn').prop('disabled', true);
+        }
+        function cancelCustom() {
+            $('#input_custom_url').hide();
+            $('#url').prop('disabled', false);
+            $('#btn').prop('disabled', false);
+        }
         document.getElementById('btn').onclick = function() {
             document.getElementById('result').style.display = 'none';
         }
