36ac6f056e7d8deb61c29f50f7cac2afe0b2e2d0
Arifia Kasastra R <arka.progammer@gmail.com>
2020-09-17 22:56:39 +0700

    Change Offline View

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
 
-<div class="card">
-    <div class="card-header">
-        <h4>Whoops</h4>
-    </div>
-    <div class="card-body">
-        <div class="empty-state" data-height="600" style="height: 600px;">
-            <img class="img-fluid" src="../assets/img/drawkit/drawkit-full-stack-man-colour.svg" alt="image">
-            <h2 class="mt-0">We can't reach the server</h2>
-            <p class="lead">
-                We were unable to reach the server, it seemed like it was sleeping, so we were reluctant to wake it up.
-            </p>
-            <a href="#" class="btn btn-warning mt-4">Try Again</a>
-            <a href="#" class="mt-4 bb">Need Help?</a>
-        </div>
-        </div>
-</div>
 
+<div class="section-body">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card border-danger mb-3 text-center">
+                    <div class="card-header">Offline</div>
+                    <div class="card-body text-danger">
+                        <h5 class="card-title">Server Tidak Ditemukan</h5>
+                        <p class="card-text">
+                            Sepertinya anda sedang Offline sehingga aplikasi tidak dapat digunakan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
